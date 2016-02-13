94cd2900df7a08f630f9898ddb3a1832fa2ffcad
Patrick Coffey <patrickcoffey48@gmail.com>
2016-02-13 15:22:37 -0800

    Cleanup publication files

--- server/publications.js
+++ server/publications.js
@@ -1,6 +1,18 @@
-Meteor.publish("chatrooms",function(){
-    return ChatRooms.find({});
+/**
+ * @file
+ * Contains all publications for this application.
+ */
+
+/**
+ * Publishes all chatroom documents.
+ */
+Meteor.publish('chatrooms', function() {
+  return ChatRooms.find({});
 });
-Meteor.publish("onlusers",function(){
-    return Meteor.users.find({"status.online":true},{username:1});
+
+/**
+ * Publishes all online users.
+ */
+Meteor.publish('onlusers', function() {
+  return Meteor.users.find({ 'status.online': true }, { username: 1 });
 })
